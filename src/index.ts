5e2a1bf2bc14bdddd6532bcc8333b9a95c141608
Lucas Eduardo <33419659+MaiaLucas@users.noreply.github.com>
2022-09-03 09:25:37 -0300

    feat: adicionando visualização das tabelas

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import Switch from "./entity/Switch";
 import Host from "./entity/Host";
 import { print } from "./utils";
 import { Constants } from "./constants";
+import IPacket from "./interface/Packet";
 
 print("INIT SYSTEM");
 const switchDevice = new Switch();
@@ -15,7 +16,7 @@ switchDevice.addLink(h1.port, 1);
 switchDevice.addLink(h2.port, 4);
 switchDevice.addLink(h3.port, 3);
 
-console.log({ target: "SWITCH", switchDevice, ports: switchDevice.ports });
+// console.log({ target: "SWITCH", switchDevice, ports: switchDevice.ports });
 //
 
 h1.addLink(switchDevice.ports[0]);
@@ -23,5 +24,14 @@ h2.addLink(switchDevice.ports[3]);
 h3.addLink(switchDevice.ports[2]);
 
 //
+const packet = {
+  payload: "OI",
+  destinationIp: `${Constants.startIp}.3`,
+} as IPacket;
+h1.send(packet);
 
-h1.send("OI", `${Constants.startIp}.3`);
+print("TABLES");
+console.log({ target: "SWITCH", table: switchDevice.forwardingTable.data });
+console.log({ target: "H1", table: h1.arpTable.data });
+console.log({ target: "H2", table: h2.arpTable.data });
+console.log({ target: "H3", table: h3.arpTable.data });
